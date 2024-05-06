178e3618eebf75fdee60fcf03da0c46e874c0409
SteveZhang <871506263@qq.com>
2024-05-06 13:51:29 +0800

    高阶组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { JSX } from 'react/jsx-runtime'
 import './App.css'
+import { useState } from 'react'
 // import CustomComp from './components/CustomComp'
 // import Child from './components/Child'
 // import Error from './components/Error'
 // import Loading from './components/Loading'
 // import { CustomCompProps } from './types'
-import Layout from './components/Layout'
-import ClassComp from './components/ClassComp'
+// import Layout from './components/Layout'
+// import ClassComp from './components/ClassComp'
+// import Form from './components/Form'
 
 /* const App: FC = () => {
   const customCompProps: CustomCompProps = { name: 'hello', age: 22 }
@@ -27,26 +30,57 @@ import ClassComp from './components/ClassComp'
   )
 } */
 
-import UserNameContext from './contexts/UserNameContext'
-interface LayoutProps {
-  name?: string
-  age?: number
+// import UserNameContext from './contexts/UserNameContext'
+// interface LayoutProps {
+//   name?: string
+//   age?: number
+// }
+
+// const App = () => {
+//   const username = '张三'
+//   // const layoutProps: LayoutProps = { name: 'hello', age: 22 }
+
+//   return (
+//     <UserNameContext.Provider value={username}>
+//       <Layout {...layoutProps} >
+//         <span className='text-red-700'>Layout children</span>
+//       </Layout>
+//       <Layout />
+//       <ClassComp />
+//     </UserNameContext.Provider>
+//   )
+// }
+
+// const App = () => {
+//   const submit = (data: any) => {
+//     console.log('父组件data: ', data)
+//   }
+//   return <Form onSubmit={submit} />
+// }
+
+function MyComponent(props: { name: string }) {
+  return <div className='p-4 bg-slate-100 text-[4rem]'>hello {props.name}</div>
 }
 
+function Loading() {
+  return <div className='p-4 bg-slate-100 text-red-600 text-[4rem]'>loading 666...</div>
+}
+
+const withLoadingFn = (MyComp: (props: { name: string }) => JSX.Element) => {
+  return (props: { isLoading: boolean }) => {
+    if (props.isLoading) return <Loading />
+    return <MyComp name='111' />
+  }
+}
+
+const WithLoadingComp = withLoadingFn(MyComponent)
 
 const App = () => {
-  const username = '张三'
-  // const layoutProps: LayoutProps = { name: 'hello', age: 22 }
-
-  return (
-    <UserNameContext.Provider value={username}>
-      {/* <Layout {...layoutProps} >
-        <span className='text-red-700'>Layout children</span>
-      </Layout> */}
-      <Layout />
-      <ClassComp />
-    </UserNameContext.Provider>
-  )
+  const [loading, setLoading] = useState(true)
+  setTimeout(() => {
+    setLoading(false)
+  }, 3000)
+  return <WithLoadingComp isLoading={loading} />
 }
 
 export default App
