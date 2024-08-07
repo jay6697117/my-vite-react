c82543aec1a8749d210bc3d0050db0ea6b812d6a
SteveZhang <871506263@qq.com>
2024-08-07 17:33:51 +0800

    1

--- src/components/Child/index.tsx
+++ src/components/Child/index.tsx
@@ -1,25 +1,29 @@
 import { useState } from 'react'
 import { Button } from 'antd'
+import { Divider } from "antd";
+import style from './index.module.less';
 
-const Child: React.FC<any> = ({ getNumber }) => {
-  const [number, setNumber] = useState<number>(0);
+const Child: React.FC<any> = props => {
+  const { flag, getNumber, children } = props
+  const [number, setNumber] = useState<number>(0)
   const handleClick = () => {
-    const newNumber = number + 10;
-    setNumber(newNumber);
-    getNumber(newNumber);
+    const newNumber = number + 10
+    setNumber(newNumber)
+    getNumber(newNumber)
   }
   return (
-    <div style={{ border: "1px solid #000", padding: 20 }}>
+    <div style={{ border: '1px solid #000', padding: 20 }}>
       <div>我是子组件</div>
-      <div>子组件的number: {number}</div>
-      <Button
-        type="primary"
-        onClick={handleClick}
-      >
+      <Divider className={style['divider-style-custom']}  />
+      <div>父组件传递的flag：{JSON.stringify(flag)}</div>
+      <div>父组件传递的children：{children}</div>
+      <Divider className={style['divider-style-custom']} />
+      <div className='mb-4'>子组件的number: {number}</div>
+      <Button type='primary' onClick={handleClick}>
         +10
       </Button>
     </div>
-  );
-};
+  )
+}
 
 export default Child
--- src/components/Parent/index.tsx
+++ src/components/Parent/index.tsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
+import { Button } from 'antd'
 import Child from '@/components/Child'
 
 const Parent: React.FC<any> = () => {
-  const [number, setNumber] = useState<number>(0);
+  const [flag, setFlag] = useState<boolean>(true)
+  const [number, setNumber] = useState<number>(0)
 
+  const handleChangeFlag = () => setFlag(v => !v)
   const handleGetNumber = (v: number) => {
-    setNumber(v);
+    setNumber(v)
   }
   return (
     <>
       <div>我是父组件</div>
+      <Button type='primary' onClick={handleChangeFlag} className='my-2'>
+        切换状态
+      </Button>
       <div>父组件的number: {number}</div>
-      <Child
-        getNumber={handleGetNumber}
-      >
-        大家好，我是小杜杜，一起玩转Hooks吧！
-      </Child>
+      <Child flag={flag} getNumber={handleGetNumber}>大家好，我是小杜杜，一起玩转Hooks吧！</Child>
     </>
-  );
-};
+  )
+}
 
 export default Parent
