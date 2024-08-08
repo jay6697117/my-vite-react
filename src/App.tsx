7d41a4d16eaffc5d3b1cf476dc1dee6a169944b0
SteveZhang <871506263@qq.com>
2024-08-08 10:56:36 +0800

    添加

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import Parent from '@/components/Parent'
-import { Divider } from 'antd'
+import { Spin } from 'antd'
 import { type CompProps } from '@/types'
 import '@/styles/App.less'
 
 // App组件
-const App: React.FC<CompProps> = (props: CompProps) => {
-  const { info, children } = props
+const App: React.FC<CompProps> = () => {
+  // 定义一个状态变量loading，用于控制Spin组件的显示
+  const [loading, setLoading] = useState(true)
+  // 使用useEffect钩子在组件挂载后设置一个定时器
+  useEffect(() => {
+    // 设置一个定时器，3秒后将loading状态设置为false
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1500)
+
+    // 清除定时器
+    return () => {
+      return clearTimeout(timer)
+    }
+  }, []) // 空依赖数组，表示只在组件挂载和卸载时执行
+
   return (
     <div className='app'>
-      <h1 className='mb-4 bg-blue-400 px-4 py-2 rounded-xl'>info: {JSON.stringify(info)}</h1>
-      {children}
-      <Divider style={{ background: 'red' }} />
-      <Parent />
+      {loading ? (
+        <div className='spin-container'>
+          <Spin size='large' />
+        </div>
+      ) : (
+        <Parent />
+      )}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@/styles/index.less'
 
-const handleClick = () => {
-  alert('点我干嘛')
-}
 // 创建根元素并渲染App组件
-createRoot(document.getElementById('root')!).render(
-  <App info={{ a: 1, b: 2 }}>
-    <button onClick={handleClick} className='bg-purple-400'>测试App的children</button>
-  </App>
-)
+createRoot(document.getElementById('root')!).render(<App />)
